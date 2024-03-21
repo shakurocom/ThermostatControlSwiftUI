7b97c8c12eac6adb00b40adba49b0a56f85c8446
Vlad <vonipko@shakuro.com>
2024-03-21 12:49:17 +0300

    II-28-[more controls]

--- ThermostatControlSwiftUI/ThermostatContentView.swift
+++ ThermostatControlSwiftUI/ThermostatContentView.swift
@@ -9,58 +9,73 @@ import SwiftUI
 
 struct ThermostatContentView: View {
 
+    @State private var isEnabled = true
+    @State private var roomName = "Living Room"
+
     var body: some View {
         VStack(spacing: 0) {
             HStack(spacing: 0) {
-                VStack {
+                VStack(alignment: .leading) {
+                    Spacer().frame(height: 30)
+
+                    // Controls
+                    VStack(alignment: .leading, spacing: 4, content: {
+                        HStack(alignment: .center) {
+                            Menu(content: {
+                                Button("Living Room") {
+                                    roomName = "Living Room"
+                                }
+                                Button("Living Room 2") {
+                                    roomName = "Living Room 2"
+                                }
+                                Button("Dining room") {
+                                    roomName = "Dining room"
+                                }
+                            }, label: {
+                                HStack(content: {
+                                    Text(roomName)
+                                        .minimumScaleFactor(0.8)
+                                        .font(Stylesheet.FontFace.SFProRoundedBold.font(16))
+                                        .foregroundColor(.white)
+                                    Spacer()
+                                    Text("􀆈")
+                                        .font(Stylesheet.FontFace.SFProRoundedBold.font(14))
+                                        .foregroundColor(.white.opacity(0.5))
+                                }).frame(maxHeight: .infinity)
+                            }).frame(maxHeight: .infinity)
+                        }
+                        .padding(.horizontal, 20)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 64)
+                        .background(.white.opacity(0.08))
+                        .cornerRadius(12)
+                        
+                        HStack(content: {
+                            Toggle("On/Of", isOn: $isEnabled)
+                                .labelsHidden()
+                                .tint(Stylesheet.Color.coldMode)
+                        })
+                        .padding(.horizontal, 20)
+                        .frame(height: 64)
+                        .background(.white.opacity(0.08))
+                        .cornerRadius(12)
+                    })
+
                     Spacer()
 
                     // Labels
-                    VStack(spacing: -12, content: {
-                        Text("°F")
-                            .font(Stylesheet.FontFace.SFProRoundedBold.font(20))
-                            .foregroundColor(.white.opacity(0.5))
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                        Text("68")
-                            .lineLimit(0)
-                            .font(Stylesheet.FontFace.SFProRoundedBold.font(104))
-                            .minimumScaleFactor(0.8)
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                    })
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .background(Color.black)
+                    makeLabels()
+                        .background(Color.black)
 
                     Spacer().frame(height: 46)
 
                     // Bottom buttons
-                    VStack(spacing: 4, content: {
-                        GlowingButton(image: Image(systemName: "a.circle"),
-                                      title: "Auto",
-                                      selectedColor: Stylesheet.Color.autoMode,
-                                      cornerRadius: 16,
-                                      animateImageOnSelectionChanged: false)
-                        .frame(width: 164, height: 80)
-                        .font(Stylesheet.FontFace.SFProRoundedBold.font(20))
-                        HStack(spacing: 4) {
-                            GlowingButton(image: Image(systemName: "snowflake"),
-                                          title: nil,
-                                          selectedColor: Stylesheet.Color.coldMode,
-                                          cornerRadius: 16,
-                                          animateImageOnSelectionChanged: true)
-                            .frame(width: 80, height: 80)
-                            GlowingButton(image: Image(systemName: "sun.max"),
-                                          title: nil,
-                                          selectedColor: Stylesheet.Color.hotMode,
-                                          cornerRadius: 16,
-                                          animateImageOnSelectionChanged: true)
-                            .frame(width: 80, height: 80)
-                        }
-                    })
-                    .background(Color.black)
+                    makeBottomButtons()
+                        .background(Color.black)
                 }
-                .frame(maxWidth: 164, maxHeight: .infinity)
-                .background(Color.red)
+                .frame(width: 164)
+                .frame(maxHeight: .infinity)
+                .background(Color.black)
 
                 VStack {
                 }
@@ -78,6 +93,54 @@ struct ThermostatContentView: View {
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .padding(EdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16))
         .background(.black)
+    }
+
+    @ViewBuilder private func makeLabels() -> some View {
+
+        VStack(spacing: -12, content: {
+            Text("°F")
+                .font(Stylesheet.FontFace.SFProRoundedBold.font(20))
+                .foregroundColor(.white.opacity(0.5))
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Text("68")
+                .lineLimit(0)
+                .font(Stylesheet.FontFace.SFProRoundedBold.font(104))
+                .minimumScaleFactor(0.8)
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Text("􁃛 56%")
+                .font(Stylesheet.FontFace.SFProRoundedMedium.font(16))
+                .foregroundColor(.white.opacity(0.5))
+                .frame(maxWidth: .infinity, alignment: .leading)
+        })
+        .frame(maxWidth: .infinity, alignment: .leading)
+
+    }
+
+    @ViewBuilder private func makeBottomButtons() -> some View {
+        VStack(spacing: 4, content: {
+            GlowingButton(image: Image(systemName: "a.circle"),
+                          title: "Auto",
+                          selectedColor: Stylesheet.Color.autoMode,
+                          cornerRadius: 16,
+                          animateImageOnSelectionChanged: false)
+            .frame(width: 164, height: 80)
+            .font(Stylesheet.FontFace.SFProRoundedBold.font(20))
+            HStack(spacing: 4) {
+                GlowingButton(image: Image(systemName: "snowflake"),
+                              title: nil,
+                              selectedColor: Stylesheet.Color.coldMode,
+                              cornerRadius: 16,
+                              animateImageOnSelectionChanged: true)
+                .frame(width: 80, height: 80)
+                GlowingButton(image: Image(systemName: "sun.max"),
+                              title: nil,
+                              selectedColor: Stylesheet.Color.hotMode,
+                              cornerRadius: 16,
+                              animateImageOnSelectionChanged: true)
+                .frame(width: 80, height: 80)
+            }
+        })
     }
 
 }
